1b5504cc4dff7928263fcb3880eb27b12983354c
avinash singh rathore <avinashrathore1.a@gmail.com>
2023-09-25 23:59:39 +0530

    context api part done

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -4,10 +4,12 @@ import toast from "react-hot-toast";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
+import { useAuth } from "../../context/auth";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [auth, setAuth] = useAuth()
 
   const navigate = useNavigate();
 
@@ -21,6 +23,12 @@ const Login = () => {
       );
       if (res.data.success) {
         toast.success(res.data.message);
+        setAuth({
+          ...auth,
+          user: res.data.user,
+          token: res.data.token,
+        })
+        localStorage.setItem('auth', JSON.stringify(res.data))
         navigate("/");
       } else {
         toast.error(res.data.message);
